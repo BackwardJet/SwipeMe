bf38edae2e245353c56f53f5fd5e07fca18a7701
tejvuligonda <tejvuligonda@gmail.com>
2017-01-02 01:43:57 -0800

    Updated home page to have CSS styling for buttons

--- LAMP/home.php
+++ LAMP/home.php
@@ -1,11 +1,13 @@
 <?php
-    echo "<!DOCTYPE html>\n<html>\n<head>\n</head>\n";
+    echo "<!DOCTYPE html>\n<html>\n<head>\n";
+    echo "\t<link rel=\"stylesheet\" href=\"public/css/main.css\">";
+    echo "</head>\n";
     echo "<body>\n";
-    echo "\t<div class=button>\n";
-    echo "\t\t<a href=\"/market.html\">I want to buy swipes</a>\n";
+    echo "\t<div>\n";
+    echo "\t<input type=\"button\" class=\"button\" onclick=\"location.href='market.html';\" value=\"I want to buy swipes\"/>";
     echo "\t</div>\n";
-    echo "\t<div class=button>\n";
-    echo "\t\t<a href=\"/market.html\">I want to sell swipes</a>\n";
+    echo "\t<div>\n";
+    echo "\t<input type=\"button\" class=\"button\" onclick=\"location.href='sellerform.html';\" value=\"I want to sell swipes\"/>";
     echo "\t</div>\n";
     echo "</body>\n</html>\n";
 ?>
