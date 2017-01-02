96084d5640f792545b119934254154f082a27dda
tejvuligonda <tejvuligonda@gmail.com>
2017-01-01 23:04:40 -0800

    Added a basic home page using PHP

---
+++ LAMP/home.php
@@ -0,0 +1,11 @@
+<?php
+    echo "<!DOCTYPE html>\n<html>\n<head>\n</head>\n";
+    echo "<body>\n";
+    echo "\t<div class=button>\n";
+    echo "\t\t<a href=\"/market.html\">I want to buy swipes</a>\n";
+    echo "\t</div>\n";
+    echo "\t<div class=button>\n";
+    echo "\t\t<a href=\"/market.html\">I want to sell swipes</a>\n";
+    echo "\t</div>\n";
+    echo "</body>\n</html>\n";
+?>
