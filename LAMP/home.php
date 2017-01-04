a26c626b980a5ed9d1b6f35193cc8a7954982f16
tejvuligonda <tejvuligonda@gmail.com>
2017-01-04 06:32:59 -0500

    Created basic 'Seller's information' page

--- LAMP/home.php
+++ LAMP/home.php
@@ -7,7 +7,7 @@
     echo "\t<input type=\"button\" class=\"button\" onclick=\"location.href='market.html';\" value=\"I want to buy swipes\"/>";
     echo "\t</div>\n";
     echo "\t<div>\n";
-    echo "\t<input type=\"button\" class=\"button\" onclick=\"location.href='sellerform.html';\" value=\"I want to sell swipes\"/>";
+    echo "\t<input type=\"button\" class=\"button\" onclick=\"location.href='sell.php';\" value=\"I want to sell swipes\"/>";
     echo "\t</div>\n";
     echo "</body>\n</html>\n";
 ?>
---
+++ LAMP/sell.php
@@ -0,0 +1,20 @@
+<?php
+    echo "<!DOCTYPE html>\n<html>\n<head>\n";
+    echo "\t<link rel=\"stylesheet\" href=\"public/css/main.css\">";
+    echo "</head>\n";
+    echo "<body>\n";
+    echo "\t<form>\n";
+    echo "\t\t<p>Name*</p>\n";
+    echo "\t\t<input type=\"text\" name=\"name\">\n";
+    echo "\t\t<p>Email*</p>\n";
+    echo "\t\t<input type=\"text\" name=\"email\">\n";
+    echo "\t\t<p>Phone Number</p>\n";
+    echo "\t\t<input type=\"text\" name=\"phonenumber\">\n";
+    echo "\t\t<p>Number of Swipes you're selling*</p>\n";
+    echo "\t\t<input type=\"text\" name=\"numswipes\">\n";
+    echo "\t\t<p>Price*</p>\n";
+    echo "\t\t<input type=\"text\" name=\"price\">\n";
+    echo "\t\t<p><input type=\"submit\"><input type=\"reset\"></p>\n";
+    echo "\t</form>\n";
+    echo "</body>\n</html>\n";
+?>
